32cfff1b639d58b92ab6421f33cb77c7df801036
anu singh <annusingh58@gmail.com>
2023-05-18 17:59:06 +0530

    updates

--- server.js
+++ server.js
@@ -1,74 +1,100 @@
-function Signup(event){
+function signup(event){
     event.preventDefault();
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var password=document.getElementById("password").value;
-    var cpassword=document.getElementById("confimpassword").value;
-
-    if(name && email && password && cpassword){
-        if(password.length>=8 && cpassword.length>=8){
-            if(password==cpassword){
-                var ls=JSON.parse(localStorage.getItem("instausers")) || [];
-                var flag=false;
-                for(var i=0;i<ls.length;i++){
-                    if(ls[i].uemail==email){
-                        flag=true;
-
-                    }
-
-                }
-                if(flag==true){
-                    alert("email exist enter new email");
-                }
-                else{
-                    var insta={uname:name,uemail:email,upassword:password,ucpassword:cpassword};
-                    ls.push(insta);
-                    localStorage.setItem("instausers",JSON.stringify(ls));
-                    alert("sign up successfully");
-
-                    window.location.href=`./login.html`
-                }
-
-            }
-            else{
-                console.log("password not matched");
-            }
-
-        }
-
-    
-
-    else{
-        console.log("password should  be more then  8 digit");
-    }
-}
-    else{
-        console.log("all field are required");
-    }
-
-}
-function login(event){
-    event.preventDefault();
-    var email=document.getElementById("lemail").value;
-    var pass=document.getElementById("lpassword").value;
-    var insta_currentuser;
-    if(email && pass){
-        var iarray=JSON.parse(localStorage.getItem("instausers"));
-        console.log(iarray,"array herre");
-        var flag=flse;
-        for(var i=0;i<iarray.length;i++){
-            if(iarray[i].uemail==email){
-                flag=true;
-                insta_currentuser=iarray[i];
-            }
-        }
-        if(flag==true){
-            localStorage.setItem("insta_currentuser",JSON.stringify(insta_currentuser));
-            alert("login successfully");
-            window.location.href="./home.html";
-        }
+    alert("working");
+ 
+ 
+    var name =document.getElementById("iname").value;
+    var email =document.getElementById("iemail").value;
+    var password=document.getElementById("ipassword").value;
+    var confimpassword=document.getElementById("iconfimpassword").value;
+ 
+    if( name && email && password && confimpassword){
+     if(password.length>=8 && confimpassword.length>=8){
+         if(password==confimpassword){
+ 
+            
+                 var ins= JSON.parse(localStorage.getItem("UserData")) || []   
+                 var flag=false;
+                 for(i=0;i<ins.length;i++){
+                     if(ins[i].uemail==email){
+                         flag=true;
+                     }
+ 
+                 }
+                 if(flag==false){
+                 var data ={uname :name,uemail:email,upassword:password,uconfimpassword:confimpassword}
+                 ins.push(data);
+                 console.log(ins);
+                 localStorage.setItem("UserData",JSON.stringify(ins))
+                 window.location.href="./login.html"
+ 
+                 }
+                 else{
+                     alert("email already exits")
+                 }
+ 
+             }
+             else{
+                 console.log("password not match");
+             }
+ 
+         }
+ 
+         else{
+             console.log("password should be 8 digit");
+            
+         }
+ 
+ 
     }
     else{
-        console.log("both fields are required");
+      console.log("all field are required")
     }
-}
+ 
+ 
+ 
+ 
+ }
+ 
+ function login(event){
+     event.preventDefault();
+     // alert("working");
+ 
+ 
+     var lemail=document.getElementById("lemail").value;
+     var lpassword=document.getElementById("lpassword").value;
+ 
+     var loginuser={};
+     if(lemail && lpassword){
+         var ins=JSON.parse(localStorage.getItem("UserData"))
+         var flag =false;
+         for(var i=0;i<ins.length;i++){
+             if(ins[i].uemail==lemail){
+                 flag=true;
+                 loginuser=ins[i];
+             }
+ 
+            
+         }
+              if( flag==true){
+                 localStorage.setItem("CurrentUser",JSON.stringify(loginuser))
+                 alert("login successfull")
+                 window.location.href="./homepage.html"
+                 
+ 
+         }
+         
+ 
+ 
+         else{
+             alert("caredential are match")
+         }
+ 
+ 
+     }
+     else{
+         alert("all field are required")
+     }
+ 
+ }
+ 
